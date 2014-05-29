d88d78f28ee78a424b837b74bdf4b2c0caa8cbd7
Félix Girault <felix.girault@gmail.com>
2014-05-29 18:10:50 +0200

    Fixed data parsing in OEmbed

--- test/services/oembed.js
+++ test/services/oembed.js
@@ -1,6 +1,7 @@
 /**
  *	@author Félix Girault <felix.girault@gmail.com>
  */
+var co = require('co');
 var should = require('should');
 var Service = require('../../lib/service');
 var OEmbed = require('../../lib/services/oembed');
@@ -48,6 +49,51 @@ describe('OEmbed', function() {
 			var endpoint = oembed._completeEndpoint('url');
 
 			endpoint.should.be.equal('url');
+		});
+	});
+
+	describe('#_parseJson', function() {
+		it('should parse a JSON string', function() {
+			co(function *() {
+				var json = { 'title': 'Title' };
+				var data = yield oembed._parseJson(JSON.stringify(json));
+
+				data.should.eql(json);
+			})();
+		});
+
+		it('should throw an error when a JSON string cannot be parsed', function(done) {
+			co(function *() {
+				try {
+					var data = yield oembed._parseJson('{,}');
+				} catch (e) {
+					done();
+				}
+			})();
+		});
+	});
+
+	describe('#_parseXml', function() {
+		it('should parse a XML string', function() {
+			co(function *() {
+				var data = yield oembed._parseXml(
+					'<oembed>'
+						+ '<title>Title</title>'
+					+ '</oembed>'
+				);
+
+				data.should.have.property('title', 'Title');
+			})();
+		});
+
+		it('should throw an error when a XML string cannot be parsed', function(done) {
+			co(function *() {
+				try {
+					yield oembed._parseXml('<>');
+				} catch (e) {
+					done();
+				}
+			})();
 		});
 	});
 });
