f5d9410c276cd7bce9a7edc2c4c3961371475348
Félix Girault <felix.girault@gmail.com>
2014-05-30 00:49:09 +0200

    Added Youtube service

---
+++ test/services/youtube.js
@@ -0,0 +1,43 @@
+/**
+ *	@author Félix Girault <felix.girault@gmail.com>
+ */
+var should = require('should');
+var Service = require('../../lib/service');
+var Youtube = require('../../lib/services/youtube');
+
+
+
+/**
+ *
+ */
+describe('Youtube', function() {
+	var youtube = null;
+
+	beforeEach(function() {
+		if (youtube) {
+			delete youtube;
+		}
+
+		youtube = new Youtube();
+	});
+
+	it('should extend Service', function() {
+		youtube.should.be.an.instanceOf(Service);
+	});
+
+	describe('#prepareUrl', function() {
+		it('should refactor invalid URLs', function() {
+			youtube.prepareUrl('http://www.youtube.com/watch?v=OKY6BGcx37k&noise=noise')
+				.should.equal('http://www.youtube.com/watch?v=OKY6BGcx37k');
+
+			youtube.prepareUrl('http://www.youtube.com/v/OKY6BGcx37k')
+				.should.equal('http://www.youtube.com/watch?v=OKY6BGcx37k');
+
+			youtube.prepareUrl('http://www.youtube.com/embed/OKY6BGcx37k')
+				.should.equal('http://www.youtube.com/watch?v=OKY6BGcx37k');
+
+			youtube.prepareUrl('hhttp://youtu.be/OKY6BGcx37k')
+				.should.equal('http://www.youtube.com/watch?v=OKY6BGcx37k');
+		});
+	});
+});
