3f7ca506ab1e4ae5daa959ffd86a226fa7c602a4
Félix Girault <felix.girault@gmail.com>
2015-11-06 20:57:45 +0100

    Implemented meta tags extractor

--- src/extractors/metaTags.js
+++ src/extractors/metaTags.js
@@ -1,14 +1,45 @@
 /**
  *
  */
+import cheerio from 'cheerio';
 
 
 
 /**
  *
  */
+function extractProperties(pattern, html) {
+	const $ = cheerio.load(html);
+	const metas = $('meta', 'head');
+	const props = {};
+
+	metas.each((i, meta) => {
+		const name = $(meta).attr('property');
+
+		if (!name || !name.match(pattern)) {
+			return;
+		}
+
+		if (!props[name]) {
+			props[name] = [];
+		}
+
+		props[name].push(
+			$(meta).attr('content')
+		);
+	});
+
+	return props;
+}
+
+/**
+ *
+ */
 export default function metaTagsExtractor(pattern) {
 	return async function extractMetaTags(req, res) {
+		const html = await req.body();
+		const props = extractProperties(pattern, html);
 
+		return res.withProps(props);
 	}
 }
