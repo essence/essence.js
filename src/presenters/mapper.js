f1696a1eb2ee94357caf78d9cf374a29413b3161
Félix Girault <felix.girault@gmail.com>
2015-11-07 22:20:04 +0100

    Added mapper

---
+++ src/presenters/mapper.js
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+import reduce from 'lodash/collection/reduce';
+
+
+
+/**
+ *
+ */
+export default function mapperPresenter(map) {
+	return function mapProps(req, res) {
+		return reduce(map, (r, to, from) => {
+			return r.has(from)
+				? r.withProp(to, r.get(from))
+				: r;
+		}, res);
+	}
+};
