3831e2330ee3b2d78d129fa0b7652b73a5c8a820
Félix Girault <felix.girault@gmail.com>
2015-11-12 18:57:13 +0100

    Fixed demo code for better debugging

--- index.js
+++ index.js
@@ -9,21 +9,13 @@ import container from './src/container';
  *
  */
 (async function() {
-	const extractor = container.get('extractor');
-	const url = 'https://www.youtube.com/watch?v=SrpeLpQWzTk';
-	let res;
-
 	try {
-		res = await extractor.extract(url);
-	} catch (e) {
-		console.error(e);
-	}
+		const extractor = container.get('extractor');
+		const url = 'https://www.youtube.com/watch?v=SrpeLpQWzTk';
+		const res = await extractor.extract(url);
 
-	console.log(res.groups(
-		'og:video:url',
-		'og:video:secure_url',
-		'og:video:type',
-		'og:video:width',
-		'og:video:height'
-	));
+		console.log(res.get('title'));
+	} catch (e) {
+		console.error(e.stack);
+	}
 }());
