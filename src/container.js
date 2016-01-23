1ed3d3a49976927078649c17a198a15c4f455c3e
Félix Girault <felix.girault@gmail.com>
2016-01-24 00:05:13 +0100

    Made Container functional

--- src/container.js
+++ src/container.js
@@ -1,6 +1,3 @@
-/**
- *
- */
 import axios from 'axios';
 import memoize from 'lodash/function/memoize';
 import Container from './Container';
@@ -22,128 +19,116 @@ import fillUrl from './presenters/fillUrl';
 /**
  *
  */
-const container = new Container();
+const container = Container()
+	.withUnique('getHeaders', () => {
+		const head = memoize(axios.head);
 
-container.setUnique('getHeaders', () => {
-	const head = memoize(axios.head);
+		return async function(url) {
+			return head(url).then(response => response.headers);
+		};
+	})
+	.withUnique('getBody', () => {
+		const get = memoize(axios.get);
 
-	return async function(url) {
-		return head(url).then(response => response.headers);
-	};
-});
-
-container.setUnique('getBody', () => {
-	const get = memoize(axios.get);
-
-	return async function(url) {
-		return get(url).then(response => response.data);
-	};
-});
-
-container.setUnique('youtubePreparator', youtubePreparator);
-
-container.setUnique('oEmbedServices', () => ({
-	'youtube': {
-		filter: /youtube\.com|youtu\.be/i,
-		endpoint: 'http://www.youtube.com/oembed?format=xml&url=:url',
-		format: FORMAT_XML
-	}
-}));
-
-container.setUnique('oEmbedKnownExtractor', () => {
-	return oEmbedKnownExtractor(
-		container.get('getBody'),
-		container.get('oEmbedServices')
-	);
-});
-
-container.setUnique('oEmbedAutoExtractor', () => {
-	return oEmbedAutoExtractor(
-		container.get('getBody')
-	);
-});
-
-container.setUnique('openGraphExtractor', () => {
-	return metaTagsExtractor(
-		container.get('getBody'),
-		/^og:/i
-	);
-});
-
-container.setUnique('openGraphMapper', () => {
-	return mapperPresenter({
-		'og:url': 'url',
-		'og:type': 'type',
-		'og:title': 'title',
-		'og:description': 'description',
-		'og:site_name': 'providerName',
-		'og:image': 'thumbnailUrl',
-		'og:image:url': 'thumbnailUrl',
-		'og:image:width': 'width',
-		'og:image:height': 'height',
-		'og:video:width': 'width',
-		'og:video:height': 'height'
+		return async function(url) {
+			return get(url).then(response => response.data);
+		};
+	})
+	.withUnique('youtubePreparator', youtubePreparator)
+	.withUnique('oEmbedServices', () => ({
+		'youtube': {
+			filter: /youtube\.com|youtu\.be/i,
+			endpoint: 'http://www.youtube.com/oembed?format=xml&url=:url',
+			format: FORMAT_XML
+		}
+	}))
+	.withUnique('oEmbedKnownExtractor', () => {
+		return oEmbedKnownExtractor(
+			container.get('getBody'),
+			container.get('oEmbedServices')
+		);
+	})
+	.withUnique('oEmbedAutoExtractor', () => {
+		return oEmbedAutoExtractor(
+			container.get('getBody')
+		);
+	})
+	.withUnique('openGraphExtractor', () => {
+		return metaTagsExtractor(
+			container.get('getBody'),
+			/^og:/i
+		);
+	})
+	.withUnique('openGraphMapper', () => {
+		return mapperPresenter({
+			'og:url': 'url',
+			'og:type': 'type',
+			'og:title': 'title',
+			'og:description': 'description',
+			'og:site_name': 'providerName',
+			'og:image': 'thumbnailUrl',
+			'og:image:url': 'thumbnailUrl',
+			'og:image:width': 'width',
+			'og:image:height': 'height',
+			'og:video:width': 'width',
+			'og:video:height': 'height'
+		});
+	})
+	.withUnique('twitterTagsExtractor', () => {
+		return metaTagsExtractor(
+			container.get('getBody'),
+			/^twitter:/i
+		);
+	})
+	.withUnique('twitterTagsMapper', () => {
+		return mapperPresenter({
+			'twitter:card': 'type',
+			'twitter:title': 'title',
+			'twitter:description': 'description',
+			'twitter:site': 'providerName',
+			'twitter:creator': 'authorName'
+		});
+	})
+	.withUnique('middlewares', () => {
+		return [
+			condition(
+				isYoutubeRequest,
+				container.get('youtubePreparator')
+			),
+			condition(
+				isEmptyResponse,
+				container.get('oEmbedKnownExtractor')
+			),
+			//	condition(
+			//		isEmptyResponse,
+			//		container.get('oEmbedAutoExtractor')
+			//	),
+			condition(
+				isEmptyResponse,
+				pipeline(
+					container.get('openGraphExtractor'),
+					container.get('openGraphMapper')
+				)
+			),
+			condition(
+				isEmptyResponse,
+				pipeline(
+					container.get('twitterTagsExtractor'),
+					container.get('twitterTagsMapper')
+				)
+			),
+			//	condition(
+			//		isYoutubeRequest,
+			//		youtubePresenter()
+			//	),
+			fillUrl
+		];
+	})
+	.withUnique('extractor', () => {
+		return extractor(
+			container.get('middlewares')
+		);
 	});
-});
-
-container.setUnique('twitterTagsExtractor', () => {
-	return metaTagsExtractor(
-		container.get('getBody'),
-		/^twitter:/i
-	);
-});
-
-container.setUnique('twitterTagsMapper', () => {
-	return mapperPresenter({
-		'twitter:card': 'type',
-		'twitter:title': 'title',
-		'twitter:description': 'description',
-		'twitter:site': 'providerName',
-		'twitter:creator': 'authorName'
-	});
-});
-
-container.setUnique('middlewares', () => {
-	return [
-		condition(
-			isYoutubeRequest,
-			container.get('youtubePreparator')
-		),
-		condition(
-			isEmptyResponse,
-			container.get('oEmbedKnownExtractor')
-		),
-		//	condition(
-		//		isEmptyResponse,
-		//		container.get('oEmbedAutoExtractor')
-		//	),
-		condition(
-			isEmptyResponse,
-			pipeline(
-				container.get('openGraphExtractor'),
-				container.get('openGraphMapper')
-			)
-		),
-		condition(
-			isEmptyResponse,
-			pipeline(
-				container.get('twitterTagsExtractor'),
-				container.get('twitterTagsMapper')
-			)
-		),
-		//	condition(
-		//		isYoutubeRequest,
-		//		youtubePresenter()
-		//	),
-		fillUrl
-	];
-});
-
-container.setUnique('extractor', () => {
-	return extractor(
-		container.get('middlewares')
-	);
-});
 
 
 
---
+++ test/Container.js
@@ -0,0 +1,81 @@
+import {expect} from 'chai';
+import Container from '../src/Container';
+
+
+
+describe('Container', function() {
+	describe('get', function() {
+		it('should return the result of a factory', function() {
+			const container = Container().with(
+				'factory',
+				() => 'result'
+			);
+
+			expect(container.get('factory'))
+				.to.equal('result');
+		});
+
+		it('should throw if a factory doesn\'t exist', function() {
+			const container = Container();
+
+			expect(() => container.get('factory'))
+				.to.throw(Error);
+		});
+	});
+
+	describe('with', function() {
+		it('should return a new Container without affecting the original one', function() {
+			const container = Container();
+			const otherContainer = container.with(
+				'factory',
+				() => 'result'
+			);
+
+			expect(() => container.get('factory'))
+				.to.throw(Error);
+
+			expect(otherContainer.get('factory'))
+				.to.equal('result');
+		});
+
+		it('should call the factory for each get()', function() {
+			let count = 0;
+			const factory = () => ++count;
+			const container = Container().with(
+				'factory',
+				factory
+			);
+
+			expect(container.get('factory')).to.equal(1);
+			expect(container.get('factory')).to.equal(2);
+		});
+	});
+
+	describe('withUnique', function() {
+		it('should return a new Container without affecting the original one', function() {
+			const container = Container();
+			const otherContainer = container.withUnique(
+				'factory',
+				() => 'result'
+			);
+
+			expect(() => container.get('factory'))
+				.to.throw(Error);
+
+			expect(otherContainer.get('factory'))
+				.to.equal('result');
+		});
+
+		it('should call the factory only once', function() {
+			let count = 0;
+			const factory = () => ++count;
+			const container = Container().withUnique(
+				'factory',
+				factory
+			);
+
+			expect(container.get('factory')).to.equal(1);
+			expect(container.get('factory')).to.equal(1);
+		});
+	});
+});
