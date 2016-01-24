ff843ec9a415848275cd4763b3a4a4a3f5bf8d67
Félix Girault <felix.girault@gmail.com>
2016-01-24 16:17:34 +0100

    Ditched webpack

---
+++ src/index.js
@@ -0,0 +1,15 @@
+export {default as Container} from './Container';
+export {default as container} from './container';
+export {default as condition} from './condition';
+export {default as pipeline} from './pipeline';
+export {default as extractor} from './extractor';
+export {default as isEmptyResponseCondition} from './conditions/isEmptyResponse';
+export {default as isYoutubeRequestCondition} from './conditions/isYoutubeRequest';
+export {default as youtubePreparator} from './preparators/youtube';
+export {default as oEmbedFormats} from './extractors/oEmbedFormats';
+export {default as oEmbedExtractor} from './extractors/oEmbed';
+export {default as oEmbedAutoExtractor} from './extractors/oEmbedAuto';
+export {default as oEmbedKnownExtractor} from './extractors/oEmbedKnown';
+export {default as metaTagsExtractor} from './extractors/metaTags';
+export {default as fillUrlPresenter} from './presenters/fillUrl';
+export {default as mapperPresenter} from './presenters/mapper';
