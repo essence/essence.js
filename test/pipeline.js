366e6edbd08e0bd9702b6142a97f13c21ecad37e
Félix Girault <felix.girault@gmail.com>
2016-01-25 22:36:22 +0100

    Added tests for pipeline

---
+++ test/pipeline.js
@@ -0,0 +1,71 @@
+import chai, {expect} from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import Errors from '../src/Errors';
+import createPipeline from '../src/pipeline';
+
+chai.use(chaiAsPromised);
+
+
+
+async function increment(payload) {
+	return {
+		...payload,
+		number: payload.number + 1
+	};
+}
+
+async function throwError() {
+	throw Error();
+}
+
+
+
+describe('pipeline', function() {
+	it('should pass a payload through the middlewares', function() {
+		const payload = {
+			number: 1
+		};
+
+		const pipeline = createPipeline(
+			increment,
+			increment,
+			increment
+		);
+
+		return expect(pipeline(payload))
+			.to.eventually.have.property('number')
+			.that.equal(4);
+	});
+
+	it('should interrupt the pipeline if a middleware throws', function() {
+		const payload = {
+			number: 1,
+			err: Errors()
+		};
+
+		const pipeline = createPipeline(
+			increment,
+			throwError,
+			increment
+		);
+
+		return expect(pipeline(payload))
+			.to.eventually.have.property('number')
+			.that.equal(2);
+	});
+
+	it('should return an error if a middleware throws', function() {
+		const payload = {
+			err: Errors()
+		};
+
+		const pipeline = createPipeline(
+			throwError
+		);
+
+		return expect(pipeline(payload))
+			.to.eventually.satisfy(({err}) => {
+				return err.count() === 1;
+			});
+	});
+});
