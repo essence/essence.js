fc5423543e72fbef4ff2c88abe3df4887e1a0515
Félix Girault <felix.girault@gmail.com>
2017-10-12 21:57:24 +0200

    Added end-to-end tests

--- test/container.js
+++ test/container.js
@@ -11,23 +11,35 @@ describe('container', function() {
 		const propMatches = (key, value) => (res) =>
 			(res.get(key) === value);
 
+		const printResponse = (res) =>
+			console.log(res.toJson(2));
+
 		const tests = [
 			{
-				name: 'a youtube video',
+				name: 'a YouTube video',
 				url: 'https://www.youtube.com/watch?v=63VbiVqkXkE',
 				test: propMatches('authorName', 'Rob Scallon')
 			},
 			{
-				name: 'a dailymotion video',
+				name: 'a Dailymotion video',
 				url: 'http://www.dailymotion.com/video/x60gir3',
 				test: propMatches('providerName', 'Dailymotion')
+			},
+			{
+				name: 'an AlloCine article',
+				url: 'http://www.allocine.fr/film/fichefilm_gen_cfilm=1975.html',
+				test: propMatches('providerName', 'AlloCiné')
+			},
+			{
+				name: 'a Twitter status',
+				url: 'https://twitter.com/_fg_/status/917007377089613825',
+				test: propMatches('authorName', 'Félix Girault')
 			}
 		];
 
 		tests.forEach(({name, url, test}) => {
 			it(`should extract info from ${name}`, function() {
 				const extract = container.get('extract');
-
 				return expect(extract(url))
 					.to.eventually.satisfy(test);
 			});
